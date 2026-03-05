58dadaa72983d064892103663666c050f2c560cd
Itzhak Levy <itzhaklevydev@gmail.com>
2026-03-05 14:03:54 +0200

    Update CSS for highlightElement

--- src/test/java/utils/HighlightUtils.java
+++ src/test/java/utils/HighlightUtils.java
@@ -7,12 +7,14 @@ import org.openqa.selenium.WebElement;
 public class HighlightUtils {
 
     public static void highlightElement(WebDriver driver, WebElement element) {
+
+        // Create a JavascriptExecutor from the WebDriver to execute JavaScript code in the context of the browser
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        // Highlight the element with a red border and yellow background
+        // Highlight the element with css parameters
         js.executeScript("arguments[0].setAttribute('style', 'border: 10px solid #0003ff;');", element);
-        // Optional: Add a small delay to make the highlight visible
+        // Add a small delay to make the highlight visible
         try {
-            Thread.sleep(500);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
