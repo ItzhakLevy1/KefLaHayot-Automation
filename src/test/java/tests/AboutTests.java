63f9e80a93d557645721f87b28da6eb21c7ab3ff
Itzhak Levy <itzhaklevydev@gmail.com>
2026-03-08 11:02:36 +0200

    test: add TC-08 to verify low color contrast of T&C label in About page -UI Accessibility: Identified a usability defect where the "Terms and Conditions" label in the newsletter section lacks sufficient contrast against the background image. -POM Implementation: Created AboutPage class with targeted locators for the newsletter subscription elements. -Modularity: Created dedicated highlight methods (highlightEmailAsterisk, highlightMailingObscurity) to allow granular bug reporting. -Code Reusability: Implemented a private scrollToAndHighlight helper to reduce boilerplate code. -Visual Validation: Integrated HighlightUtils and proactive screenshots to document the obscured text for the Allure report. -Metadata: Applied Allure annotations (@feature, @Severity, @owner) to categorize the defect under "Registration UI" for consistency.

---
+++ src/test/java/tests/AboutTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import io.qameta.allure.*;
+import org.testng.annotations.Test;
+import pages.AboutPage;
+import pages.components.CookieBannerComponent;
+import utils.AllureAttachments;
+
+public class AboutTests extends BaseTest{
+
+    @Test
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("About Page UI") // Classification by feature
+    @Owner("Itzhak Levy") // Responsible for this test case
+    @Description("TC-08: Verify T&C label has poor color contrast against newsletter background image")
+    public void AboutTermsAndConditionsTest() throws InterruptedException {
+
+        AboutPage aboutPage = new AboutPage(driver);
+        CookieBannerComponent cookieBannerComponent = new CookieBannerComponent(driver);
+        aboutPage.navigateToAboutPage();
+        cookieBannerComponent.acceptCookies();
+        aboutPage.highlightTermsContrast();
+
+        Thread.sleep(2000);
+
+        AllureAttachments.saveScreenshot("About Page UI - Verify T&C label has poor color contrast against newsletter background image", driver);
+    }
+}
